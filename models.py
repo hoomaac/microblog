0c9347dfc5c3b6af01b7cdba921f4cc233a31f46
Humac <hoomaac@gmail.com>
2018-09-16 11:53:00 +0430

    post model added

--- models.py
+++ models.py
@@ -19,6 +19,15 @@ class User(UserMixin, Model):
         database = DATABASE
         order_by = ('-joined_at',)
 
+
+    def get_posts(self):
+        return Post.select().where(Post.user == self)
+
+    def get_stream(self):
+        return Post.select().where(
+            (Post.user == self)
+        )
+
     
     @classmethod
     def create_user(cls, username, email, password, admin=False):
@@ -31,6 +40,24 @@ class User(UserMixin, Model):
 
         except IntegrityError:
             raise ValueError("User exists already")
+
+
+
+class Post(Model):
+
+    timestamp = DateTimeField(default=datetime.datetime.now)
+    user = ForeignKeyField(
+        rel_model=User,
+        related_name='posts',
+    )
+
+    content = TextField()
+
+
+    class Meta:
+        database = DATABASE
+        order_by = ('-timestamp',)
+
 
 
 def initialise():
